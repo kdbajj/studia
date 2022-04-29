3f1c7d252197308f92e51a76f4736da9b450f71f
kdbajj <k.dbaj@vp.pl>
2022-04-29 15:09:53 +0200

    zmiany w kodzie

--- programowanie 2/KarolinaDbaj_kosmetyki/KarolinaDbaj_Kosmetyki/AkcesoriaKosmetyczne.cs
+++ programowanie 2/KarolinaDbaj_kosmetyki/KarolinaDbaj_Kosmetyki/AkcesoriaKosmetyczne.cs
@@ -9,96 +9,103 @@ using System.Windows.Forms;
 
 namespace KarolinaDbaj_Kosmetyki
 {
-    //deklarowanie zmiennych w kasie AkcesoriaKosmetyczne
-    internal class AkcesoriaKosmetyczne
+    class AkcesoriaKosmetyczne : Kosmetyk
     {
-
-        int numer;
-        string nazwaMarki;
-        string rodzajKosmetyku;
         int waga;
-        float cena;
-        int kodProduktu;
-        string działanie;
         string materiałWykonania;
         bool przyjazneŚrodowisku;
-        string dlaKogo;
-        int liczbaKosmetykow = 0;
-        ArrayList odcienieSzczotki = new ArrayList();
-       
+        string doCzegoSłuży;
+            //deklarowanie zmiennych w kasie AkcesoriaKosmetyczne
+            /*internal class AkcesoriaKosmetyczne
+            {
 
-        public AkcesoriaKosmetyczne()
-        {
-            liczbaKosmetykow += 1;
-            this.numer = liczbaKosmetykow;
-            this.nazwaMarki = "Tangle teezer";
-            this.rodzajKosmetyku = "Sczotka do włosów";
-            this.waga = 90;
-            this.cena = 39.99f;
-            this.kodProduktu = 189;
-            this.działanie = "Dobrze rozczesuje włosy, masuje skórę głowy";
-            this.materiałWykonania = "Tworzywo sztuczne";
-            this.przyjazneŚrodowisku = true;
-            this.dlaKogo = "Włosy normalne";
-            this.odcienieSzczotki.Add("czerwony");
-            this.odcienieSzczotki.Add("różowy");
-            this.odcienieSzczotki.Add("fioletowy");
+                int numer;
+                string nazwaMarki;
+                string rodzajKosmetyku;
+                int waga;
+                float cena;
+                int kodProduktu;
+                string działanie;
+                string materiałWykonania;
+                bool przyjazneŚrodowisku;
+                string dlaKogo;
+                int liczbaKosmetykow = 0;
+                ArrayList odcienieSzczotki = new ArrayList();
 
-        }
-        public AkcesoriaKosmetyczne(int numer, string nazwaMarki, string rodzajKosmetyku, int waga, float cena, int kodProduktu, string działanie, bool testowanyDermatologicznie, string materiałWykonania, bool przyjazneŚrodowisku, string dlaKogo)
-        {
-            liczbaKosmetykow += 1;
-            this.numer = liczbaKosmetykow;
-            this.nazwaMarki = "Tangle teezer";
-            this.rodzajKosmetyku = "Sczotka do włosów";
-            this.waga = 90;
-            this.cena = 39.99f;
-            this.kodProduktu = 189;
-            this.działanie = "Dobrze rozczesuje włosy, masuje skórę głowy";
-            this.materiałWykonania = "Tworzywo sztuczne";
-            this.przyjazneŚrodowisku = true;
-            this.dlaKogo = "Włosy normalne";
-            this.odcienieSzczotki.Add("czerwony");
-            this.odcienieSzczotki.Add("różowy");
-            this.odcienieSzczotki.Add("fioletowy");
-        }
-        public AkcesoriaKosmetyczne(AkcesoriaKosmetyczne produkt)
-        {
-            liczbaKosmetykow += 1;
-            this.numer = produkt.liczbaKosmetykow;
-            this.nazwaMarki = produkt.nazwaMarki;
-            this.rodzajKosmetyku = produkt.rodzajKosmetyku;
-            this.waga = produkt.waga;
-            this.cena = produkt.cena;
-            this.kodProduktu =produkt.kodProduktu;
-            this.działanie = produkt.działanie;
-            this.materiałWykonania = produkt.materiałWykonania;
-            this.przyjazneŚrodowisku = produkt.przyjazneŚrodowisku;
-            this.dlaKogo = produkt.dlaKogo;
-            this.odcienieSzczotki.Add("czerwony");
-            this.odcienieSzczotki.Add("różowy");
-            this.odcienieSzczotki.Add("fioletowy");
-        }
-        //dodanie do listy poszczególnych produktów
-        public void Wypisz(ListBox lblAkcesoria)
-        {
-            var kosmetyk = new Kosmetyk();
-            lblAkcesoria.Items.Add("Numer artykułu (kolejno):\t" + numer);
-            lblAkcesoria.Items.Add("Nazwa marki:" + nazwaMarki);
-            lblAkcesoria.Items.Add("Rodzaj kosmetyku:" + rodzajKosmetyku);
-            lblAkcesoria.Items.Add("Waga produktu: " + waga);
-            lblAkcesoria.Items.Add("Cena produktu: " + cena);
-            lblAkcesoria.Items.Add("Kod produktu: " + kodProduktu + kosmetyk.NowyProdukt());//metoda z klasy kosmetyki
-            lblAkcesoria.Items.Add("Działanie: " + działanie);
-            lblAkcesoria.Items.Add("Materiał wykonania: " + materiałWykonania);
-            lblAkcesoria.Items.Add("Przyjazne środowisku: " + przyjazneŚrodowisku+PrzyjazneŚrodowisku());//dodanie metoda, która wyświetla komunikat, czy produkt jest ekologiczny
-            lblAkcesoria.Items.Add("Dla kogo przeznaczony jest produkt: " + dlaKogo);
-             lblAkcesoria.Items.Add("Dostępne kolory produktu:" + odcienieSzczotki[0]);
-           lblAkcesoria.Items.Add("Dostępne kolory produktu:" + odcienieSzczotki[1]);
-            lblAkcesoria.Items.Add("Dostępne kolory produktu:" + odcienieSzczotki[2]);
-        }
-        //stworzenie metody odpowiadającej za sprawdzenie, czy produkt jest ekologiczny
-        private int PrzyjazneŚrodowisku()
+
+                public AkcesoriaKosmetyczne()
+                {
+                    liczbaKosmetykow += 1;
+                    this.numer = liczbaKosmetykow;
+                    this.nazwaMarki = "Tangle teezer";
+                    this.rodzajKosmetyku = "Sczotka do włosów";
+                    this.waga = 90;
+                    this.cena = 39.99f;
+                    this.kodProduktu = 189;
+                    this.działanie = "Dobrze rozczesuje włosy, masuje skórę głowy";
+                    this.materiałWykonania = "Tworzywo sztuczne";
+                    this.przyjazneŚrodowisku = true;
+                    this.dlaKogo = "Włosy normalne";
+                    this.odcienieSzczotki.Add("czerwony");
+                    this.odcienieSzczotki.Add("różowy");
+                    this.odcienieSzczotki.Add("fioletowy");
+
+                }
+                public AkcesoriaKosmetyczne(int numer, string nazwaMarki, string rodzajKosmetyku, int waga, float cena, int kodProduktu, string działanie, bool testowanyDermatologicznie, string materiałWykonania, bool przyjazneŚrodowisku, string dlaKogo)
+                {
+                    liczbaKosmetykow += 1;
+                    this.numer = liczbaKosmetykow;
+                    this.nazwaMarki = "Tangle teezer";
+                    this.rodzajKosmetyku = "Sczotka do włosów";
+                    this.waga = 90;
+                    this.cena = 39.99f;
+                    this.kodProduktu = 189;
+                    this.działanie = "Dobrze rozczesuje włosy, masuje skórę głowy";
+                    this.materiałWykonania = "Tworzywo sztuczne";
+                    this.przyjazneŚrodowisku = true;
+                    this.dlaKogo = "Włosy normalne";
+                    this.odcienieSzczotki.Add("czerwony");
+                    this.odcienieSzczotki.Add("różowy");
+                    this.odcienieSzczotki.Add("fioletowy");
+                }
+                public AkcesoriaKosmetyczne(AkcesoriaKosmetyczne produkt)
+                {
+                    liczbaKosmetykow += 1;
+                    this.numer = produkt.liczbaKosmetykow;
+                    this.nazwaMarki = produkt.nazwaMarki;
+                    this.rodzajKosmetyku = produkt.rodzajKosmetyku;
+                    this.waga = produkt.waga;
+                    this.cena = produkt.cena;
+                    this.kodProduktu =produkt.kodProduktu;
+                    this.działanie = produkt.działanie;
+                    this.materiałWykonania = produkt.materiałWykonania;
+                    this.przyjazneŚrodowisku = produkt.przyjazneŚrodowisku;
+                    this.dlaKogo = produkt.dlaKogo;
+                    this.odcienieSzczotki.Add("czerwony");
+                    this.odcienieSzczotki.Add("różowy");
+                    this.odcienieSzczotki.Add("fioletowy");
+                }
+                //dodanie do listy poszczególnych produktów
+                public void Wypisz(ListBox lblAkcesoria)
+                {
+                    var kosmetyk = new Kosmetyk();
+                    lblAkcesoria.Items.Add("Numer artykułu (kolejno):\t" + numer);
+                    lblAkcesoria.Items.Add("Nazwa marki:" + nazwaMarki);
+                    lblAkcesoria.Items.Add("Rodzaj kosmetyku:" + rodzajKosmetyku);
+                    lblAkcesoria.Items.Add("Waga produktu: " + waga);
+                    lblAkcesoria.Items.Add("Cena produktu: " + cena);
+                    lblAkcesoria.Items.Add("Kod produktu: " + kodProduktu + kosmetyk.NowyProdukt());//metoda z klasy kosmetyki
+                    lblAkcesoria.Items.Add("Działanie: " + działanie);
+                    lblAkcesoria.Items.Add("Materiał wykonania: " + materiałWykonania);
+                    lblAkcesoria.Items.Add("Przyjazne środowisku: " + przyjazneŚrodowisku+PrzyjazneŚrodowisku());//dodanie metoda, która wyświetla komunikat, czy produkt jest ekologiczny
+                    lblAkcesoria.Items.Add("Dla kogo przeznaczony jest produkt: " + dlaKogo);
+                     lblAkcesoria.Items.Add("Dostępne kolory produktu:" + odcienieSzczotki[0]);
+                   lblAkcesoria.Items.Add("Dostępne kolory produktu:" + odcienieSzczotki[1]);
+                    lblAkcesoria.Items.Add("Dostępne kolory produktu:" + odcienieSzczotki[2]);
+                }
+            */
+            //stworzenie metody odpowiadającej za sprawdzenie, czy produkt jest ekologiczny
+            private int PrzyjazneŚrodowisku()
         {
             if (przyjazneŚrodowisku==true)
             {
