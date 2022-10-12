429b5f493760c5b07cd0244842ba17f244863e44
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-12 14:59:50 +0200

    aktualizacja zadań

---
+++ metody probabilistyczne i statystyka/lista-2/zadanie do analizy.R
@@ -0,0 +1,12 @@
+N<-10000
+wyniki<-rep(0,N)
+for(i in 1:N){
+  wyniki[i]<-sample(c(-1,1),1)
+}
+
+mean (wyniki)
+plot(wyniki, pch=20)
+
+konto <- cumsum(wyniki)
+plot(konto,type="1")
+
--- metody probabilistyczne i statystyka/lista-2/zadanie-5.R
+++ metody probabilistyczne i statystyka/lista-2/zadanie-5.R
@@ -1,1 +1,15 @@
 # ll-runif - random uniform (losowanie liczb z przedziału)
+N - 1000
+licznik<-0
+
+for(i in 1:N){
+  LL<-runif(1, 0, 10)
+  Pole<-LL * (10 - LL)
+  
+  if(Pole <=21){
+    licznik <- licznik+1
+  }
+}
+
+licznik/N
+
