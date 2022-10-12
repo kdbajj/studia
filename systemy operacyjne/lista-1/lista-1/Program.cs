06072797b8d338f22006290da2a544552195a18b
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-12 18:07:03 +0200

    join

--- systemy operacyjne/lista-1/lista-1/Program.cs
+++ systemy operacyjne/lista-1/lista-1/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 /*Zadanie 1
  * W funkcji main należy wywołać funkcję print squares, wypisuje kwadraty kolejnych liczb naturalnych od 1 do 100.
  * Poniżej tej funkcji należy wypisać 100 razy "Hello my friend". Program ma zakończyć się po naciśnięciu esc.
  * 
- Zadanie 2
-dorzucamy nowy wątek w wątku głównym (wywołana w main ale nowy wątek)*/
+Zadanie 2
+dorzucamy nowy wątek w wątku głównym (wywołana w main ale nowy wątek)
+
+Zadanie 3
+*/
+
 
 namespace lista_1
 {
@@ -18,21 +25,45 @@ namespace lista_1
     {
         static void Main(string[] args)
         {
-            printSquares();
-           
 
+            Thread thr = new Thread(new ThreadStart(PrintSquares));
+            thr.Start();
+            thr.Join();
+
+            PrintQubes();
+
+            PrintHelloMyFriend();
+
+            Console.ReadKey();
         }
-        public static void printSquares()
+        public static void PrintSquares()
         {
-            int k = 0;
-            for (int i = 0; i < 100; i++)
+            
+            for (int i = 0; i <= 100; i++)
             {
                 Console.WriteLine("Kwadrat " + i + " to:  " + i*i);
-                
-                k++;
-                Console.WriteLine( k+ ". Hello my friend!");
+            }
+           
+        }
+
+        public static void PrintHelloMyFriend()
+        {
+            for(int i = 0; i < 100; i++)
+            Console.WriteLine(i + ". Hello my friend!");
+            while
+            (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+
+        public static void PrintQubes()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine("Sześcian " + i + " to:  " + i * i * i);
+
+
             }
         }
+
     }
 }
 
