abd7deed89a9fe2ec7e51fdd011982117c3a62a2
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-25 18:03:27 +0200

    shell sort

--- algorytmy i struktury danych/Lista 2/ulepszoneSortowanie.js
+++ algorytmy i struktury danych/Lista 2/ulepszoneSortowanie.js
@@ -1,33 +1,33 @@
 let liczbaPorownan = 0;
 
-function sortowanie(array) {
-  for (let i = 1; i < array.length; i++) {
-    // O(n)
-    let current = array[i]; // O(1)
-    let j = i - 1; // O(1)
-    while (j >= 0 && array[j] > current) {
-      // O(n)
+function shellSort(array) {
+  let n = array.length;
 
-      array[j + 1] = array[j]; // O(1) // wstawienie zmiennej do miejsca w tablicy gdy jest większa od current
-      j--; // O(1)
-      liczbaPorownan++; // O(1)
+  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    for (let i = gap; i < n; i++) {
+      let temp = array[i];
+
+      let j;
+      for (j = i; j >= gap && array[j - gap] < temp; j -= gap) {
+        liczbaPorownan++;
+        array[j] = array[j - gap];
+      }
+      array[j] = temp;
     }
-    array[j + 1] = current; // O(1)
   }
   console.log(
     `Liczba wykonanych porównań: ${liczbaPorownan},
-  natomiast wartość a wynosi: ${liczbaPorownan / Math.pow(array.length, 2)} :))`
+    natomiast wartość a wynosi: ${
+      liczbaPorownan / Math.pow(array.length, 2)
+    } :))`
   );
   return array;
 }
-console.log(sortowanie([8, 2, 4, 1, 3]));
+
+console.log(shellSort([8, 2, 4, 1, 3, 5, 6, 4, 3, 4, 5]));
 
 // Złożoność algorytmiczna
 // T: O(n^2) - czasowa (time)
 // S: O(n) - wielkościowa (space)
 
 // n = array.length
-
-//1.(2,8,4,1,3)
-//2.(8,8,4,1,3)
-//3.
--- algorytmy i struktury danych/Lista-1/sortowanie.js
+++ algorytmy i struktury danych/Lista-1/sortowanie.js
@@ -20,7 +20,7 @@ function sortowanie(array) {
   );
   return array;
 }
-console.log(sortowanie([8, 2, 4, 1, 3]));
+console.log(sortowanie([8, 2, 4, 1, 3, 5, 6, 4, 3, 4, 5]));
 
 // Złożoność algorytmiczna
 // T: O(n^2) - czasowa (time)
