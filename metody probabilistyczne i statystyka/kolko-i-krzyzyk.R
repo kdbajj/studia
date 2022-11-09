be480ff49ce9c378d05674f54d4714875180580c
Karolina Dbaj <k.dbaj@vp.pl>
2022-11-09 13:19:41 +0100

    update

--- metody probabilistyczne i statystyka/kolko-i-krzyzyk.R
+++ metody probabilistyczne i statystyka/kolko-i-krzyzyk.R
@@ -1,28 +1,96 @@
-Powt<-10000
+# encoding: UTF-8
 
-Pole<-matrix(rep(0,9),nrow=3)
-Pole[1,]<-1
-zajPola<-0
-gracz<-1;N<-0
+Powt<-10000              # Ustalamy liczbę partii
+gr1<-0                  # wyzeruj liczbę zwycięstw gracza 1
 
-while(zajPola<9){
-  xx<-sample(1:3,1); yy<-sample(1:3,1)
-  if(Pole[yy,xx]==0){
-    Pole[yy,xx]<-gracz
-    zajPola<zajPola+1
-  }
-  s1<-sum(rowSums(Pole)==3)+
-    sum(colSums(Pole)==3)+
-    sum(sum(diag(Pole))==3)+
-    sum(sum(diag(apply(Pole,2,rev)))==3)
+# utwórz tablicę do zapisu liczby posunięć w poszczególnych partiach
+dl_partii<-rep(0,Powt)  
+
+# utwórz tablicę do zapisu liczby zajętych pól w poszczeg. partiach
+zaj_pola<-rep(0,Powt)
+
+for(i in 1:Powt){
+  #---------------------------------------------------------
+  # Kod procedury symulującej jedną partię (O i X)  
+  Pole<-matrix(rep(0,9),nrow=3)  # Wyzeruj planszę 3x3 
+  gracz<-1;     # gracze będą stawiali na planszy 1 i -1
+  zap_pola<-0;  # wyzeruj licznik zapełnionych pól
+  N<-0          # wyzeruj licznik posunięć w partii
   
-  s2<-sum(rowSums(Pole)==-3)+
-    sum(colSums(Pole)==-3)+
-    sum(sum(diag(Pole))==-3)+
-    sum(sum(diag(apply(Pole,2,rev)))==-3) 
-  if(S1+S2>0)break
-  gracz<- -gracz
+  
+  wiersze <- 1
+  kolumny <- 1
+  
+  
+  while(zap_pola<9){  # dopóki są wolne pola wykonuj
+    N<-N+1      # uaktualnij numer posunięcia
+    
+    # sprawdź który gracz gra
+    if(gracz == -1){
+      # jeśli gracz -1 gra
+      
+      if(Pole[kolumny, wiersze]==0){
+        Pole[kolumny, wiersze] <- gracz
+        zap_pola<-zap_pola+1
+      }
+      
+      wiersze = wiersze + 1;
+      
+      if(wiersze > 3){
+        wiersze <- 1
+        kolumny = kolumny + 1;  
+      }
+      
+    } else{
+      # wylosuj współrzędne
+      xx<-sample(1:3,1)
+      yy<-sample(1:3,1)
+      
+      if(Pole[yy,xx]==0){
+        Pole[yy,xx]<-gracz;
+        zap_pola<-zap_pola+1
+      }
+    }
+    
+    # sprawdzanie wygranej
+    # policz, ile jest układów trzech 1 w jednej linii
+    # poziomej, pionowej lub ukośnej
+    S1<-sum(rowSums(Pole)==3)+sum(colSums(Pole)==3)+
+      sum(sum(diag(Pole))==3)+
+      sum(sum(diag(apply(Pole,2,rev)))==3)
+    # policz, ile jest układów trzech -1 w jednej linii
+    # poziomej, pionowej lub ukośnej    
+    S2<-sum(rowSums(Pole)==-3)+sum(colSums(Pole)==-3)+
+      sum(sum(diag(Pole))==-3)+
+      sum(sum(diag(apply(Pole,2,rev)))==-3)
+    if(S1+S2>0) break # jeśli pojawił się układ potrójny,
+    # to przerwij partię
+    gracz<- -gracz    # ruch otrzymuje przeciwnik
+  }
+  #---------------------------------------------------------
+  if(gracz==1) gr1<-gr1+1 # jeśli wygrał gracz 1, zwiększ
+  # jego licznik
+  dl_partii[i]<-N        # zapisz liczbę posunięć
+  # w i-tej partii
+  zaj_pola[i]<-zap_pola   # zapisz liczbę zajętych pól
+  # w i-tej partii
 }
 
- 
 Pole
+
+# wypisz ułamek zwycięstw 1 gracza (czyli przybliżone
+# prawdopodobieństwo zwycięstwa 1 gracza)
+prawdop_wygrania_gr1 <- gr1/Powt
+
+# narysuj histogram ("rozkład") liczby zajętych pól
+hist(zaj_pola)
+
+# narysuj histogram liczby posunięć w partiach
+hist(dl_partii, breaks=40)
+
+
+# wnioski:
+# 1. Rozkład liczby zajętych pól bardzo przypomina rozkład
+#    liczby posunięć w partiach. W każdym przypadku to rozkład
+#    Poissona.
+# 2. Prawdopodobieństwo wygranej gracza nr 1 jest małe - 0.3
