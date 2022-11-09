26185c21f8192d64230dc4398cc6e10cff8ca62b
Karolina Dbaj <k.dbaj@vp.pl>
2022-11-09 23:19:29 +0100

    laboratoria

---
+++ systemy operacyjne/zadanie3/zadanie3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace zadanie3
+{
+    internal class Program
+    {
+        static readonly List<int> List = Enumerable.Repeat(0, 100).ToList();
+        static void Main(string[] args)
+
+        {
+            var th = new Thread(AddElements);
+            var th2 = new Thread(SubstractElements);
+
+
+            th.Start();
+            th2.Start();
+
+            th.Join();
+            th2.Join();
+            
+
+
+        }
+        private static void AddElements()
+        {
+            int loop = 10000;
+
+            Random random = new Random();
+
+            for (int i = 0; i < loop; i++)
+            {
+                int randomNumber = random.Next(0, 100);
+                List[randomNumber] += 1;
+            }
+        }
+        private static void SubstractElements()
+        {
+            int loop = 10000;
+
+            Random random = new Random();
+
+            for (int i = 0; i < loop; i++)
+            {
+                int randomNumber = random.Next(0, 100);
+                List[randomNumber] -= 1;
+            }
+        }
+    }
+}
