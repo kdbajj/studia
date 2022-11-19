7f695588709404700cca7e9ad1f8d29d28014656
Karolina Dbaj <k.dbaj@vp.pl>
2022-11-19 15:22:03 +0100

    projekt nr 2

--- metody probabilistyczne i statystyka/SP02.R
+++ metody probabilistyczne i statystyka/SP02.R
@@ -1,4 +1,9 @@
 #1.1
-Rzut <-round(runif(10000, min=1, max=6))
-r <- hist(Rzut)
+N <- 10000
+kostka <- list(1:6)
+#kostka <- list(1,2,3,4,5,6)
+# <- runif(kostka)
+#r <- hist(Rzut)
+r <- kostka[sample(6:length(kostka), 1)]
+hist(r)
 #1.2
