725f8e34f0902389061cfd6691175a11cdc83d55
Karolina Dbaj <k.dbaj@outlook.com>
2023-03-12 23:21:32 +0100

    zadania 1-6

---
+++ Java/Lista1/src/Nauczyciel.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Nauczyciel {
+    public static void main(String[] args) {
+        Uczen uczen1 = new Uczen();//inicjalizacja klasy
+        uczen1.setImie("ania");
+        Uczen.numerKursu = 222;
+        uczen1.setOcena(5);
+        Uczen uczen2 = new Uczen();
+        uczen2.setImie("ala");
+        uczen2.setOcena(4);
+        Uczen uczen3 = new Uczen("karolina");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(uczen3.getImie());
+        System.out.println(uczen1.getImie());
+        System.out.println(uczen1.numerKursu);
+        System.out.println(uczen1.getOcena());
+        System.out.println(uczen2.getImie());
+        System.out.println(uczen2.numerKursu);
+        System.out.println(uczen2.getOcena());
+    }
+    //6.
+    //dzięki static zyskaliśmy możliwość odwoływania się do zmiennej bez
+    //inicjalizacji klasy
+
+    //dzięki akcesorowi i mutatorowi możemy odwołać się do instancji
+    //zmiennej
+
+    // dzięki final zmienna pozostaje taka sama
+
+}
+
+
