3689d06dbb1655cc372b9ab87c64df5df4574d1a
Karolina Dbaj <k.dbaj@outlook.com>
2023-04-17 20:02:40 +0200

    Lista 4 zadanie 4

---
+++ Java/Lista4-KarolinaDbaj/Zadanie4/src/TimeDifference.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.TimeZone;
+
+public class TimeDifference {
+    public static void main(String[] args) {
+        // Ustawienia stref czasowych i daty
+        TimeZone timeZone1 = TimeZone.getTimeZone("Europe/Warsaw");
+        TimeZone timeZone2 = TimeZone.getTimeZone("America/New_York");
+        int year = 2023;
+        int month = 4;
+        int day = 16;
+
+        // Obliczanie różnicy czasu w ms
+        long millis1 = timeZone1.getRawOffset() + timeZone1.getDSTSavings();
+        long millis2 = timeZone2.getRawOffset() + timeZone2.getDSTSavings();
+        long diffMillis = Math.abs(millis1 - millis2);
+
+        // Konwersja do godzin
+        double diffHours = (double)diffMillis / (60 * 60 * 1000);
+
+        System.out.println("Różnica czasu między " + timeZone1.getID() + " a " + timeZone2.getID() +
+                " wynosi " + diffHours + " godzin.");
+    }
+}
