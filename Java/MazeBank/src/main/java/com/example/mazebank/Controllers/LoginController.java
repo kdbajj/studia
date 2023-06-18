4f116e4222891b0b07cd0c85dd4ae6d8bc5f3989
Karolina Dbaj <k.dbaj@outlook.com>
2023-06-18 04:06:08 +0200

    created and developed bank app

--- Java/MazeBank/src/main/java/com/example/mazebank/Controllers/LoginController.java
+++ Java/MazeBank/src/main/java/com/example/mazebank/Controllers/LoginController.java
@@ -1,4 +1,13 @@
 package com.example.mazebank.Controllers;
 
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public Label payee_address_lbl;
+    public ChoiceBox acc_selector;
+    public TextField payee_address_fld;
+    public Label password_lbl;
+    public TextField password_fld;
 }
--- Java/MazeBank/src/main/java/module-info.java
+++ Java/MazeBank/src/main/java/module-info.java
@@ -1,9 +1,15 @@
 module com.example.mazebank {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires com.dlsc.formsfx;
+    requires java.sql;
+
 
     opens com.example.mazebank to javafx.fxml;
     exports com.example.mazebank;
+    exports com.example.mazebank.Controllers;
+    exports com.example.mazebank.Controllers.Admin;
+    exports com.example.mazebank.Controllers.Client;
+    exports com.example.mazebank.Models;
+    exports com.example.mazebank.Views;
 }
