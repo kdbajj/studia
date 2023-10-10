0b51024d06611ec2643dca28a93595137b9da358
Karolina Dbaj <k.dbaj@outlook.com>
2023-10-10 10:20:26 +0200

    updated Analiza metod numerycznych - Lista 1 zadanie 5

--- praktyka metod numerycznych/Lista1/Zadanie5/Program.cs
+++ praktyka metod numerycznych/Lista1/Zadanie5/Program.cs
@@ -4,30 +4,34 @@ class Program
 {
     static void Main()
     {
-        double x = 1.0;  // Wartość, dla której chcesz obliczyć e^x
+        double x = 5.0;  // Wartość dla której obliczamy e^x
         double epsilon = 0.0001;  // Tolerancja błędu
-        double result = CalculateExponential(x, epsilon);
+
+        if (x > 0)
+        {
+            Console.WriteLine("Błąd jest względny");
+        }
+        else if (x < 0)
+        {
+            Console.WriteLine("Błąd jest bezwzględny");
+        }
+
+
+        double result = CalculateExponentialWzglednego(x, epsilon);
         Console.WriteLine(result);
     }
 
-    static double CalculateExponential(double x, double epsilon)
+    static double CalculateExponentialWzglednego(double x, double epsilon)
     {
-        double sum = 1.0;  // Początkowa suma (pierwszy składnik szeregu Taylora)
-        double term = 1.0;  // Początkowy składnik szeregu Taylora
-        int n = 1;  // Licznik składników
+        double sum = 1.0;
+        double term = x;
+        int n = 1;
 
-        // Kontynuuj dodawanie składników, aż różnica będzie mniejsza niż epsilon
-        while (true)
+        while (Math.Abs(term) >= epsilon)
         {
-            term *= x / n;  // Oblicz kolejny składnik szeregu
-            double newSum = sum + term;  // Oblicz nową sumę
-
-            // Sprawdź, czy różnica jest mniejsza niż epsilon
-            if (Math.Abs(newSum - sum) < epsilon)
-                break;
-
-            sum = newSum;  // Zaktualizuj sumę
-            n++;  // Zwiększ licznik składników
+            sum += term;
+            n++;
+            term *= x / n;
         }
 
         return sum;
